c2b85b4e9e1c8b30667304dd18cfc0e49a5a31db
Vela <velaoc@proton.me>
2026-08-19 00:33:22 -0600

    remove app/controllers/foundation/home_controller.rb

--- app/controllers/foundation/home_controller.rb
+++
@@ -1,8 +0,0 @@
-module Foundation
-  # Minimal landing page so the app has a root to sign in/out against.
-  # Replaced by the marketing set in SPEC M7.8.
-  class HomeController < ApplicationController
-    def show
-    end
-  end
-end
